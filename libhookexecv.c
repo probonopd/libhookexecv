6eb26461e4b31e078dd43c14269eca4141c0dbca
probonopd <probonopd@users.noreply.github.com>
2018-12-18 07:58:50 +0100

    Update libhookexecv.c
    https://github.com/Hackerl/Wine_Appimage/issues/11#issuecomment-447834456

--- libhookexecv.c
+++ libhookexecv.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 /* Author: https://github.com/Hackerl/
- * https://github.com/Hackerl/Wine_Appimage/issues/11#issuecomment-445724165
+ * https://github.com/Hackerl/Wine_Appimage/issues/11#issuecomment-447834456
  * sudo apt-get -y install gcc-multilib
  * gcc -shared -fPIC -ldl libhookexecv.c -o libhookexecv.so
  * for i386: gcc -shared -fPIC -m32 -ldl libhookexecv.c -o libhookexecv.so
@@ -35,7 +35,7 @@ int execv(char *path, char ** argv)
 
     char * wineloader = getenv("WINELDLIBRARY");
 
-    if (wineloader == NULL || strendswith(path, "wine-preloader"))
+    if (wineloader == NULL)
     {
         return old_execv(path, argv);
     }
@@ -45,10 +45,21 @@ int execv(char *path, char ** argv)
     char ** new_argv = (char **) malloc( (last_arg - argv + 2) * sizeof(*argv) );
     memcpy( new_argv + 1, argv, (last_arg - argv + 1) * sizeof(*argv) );
 
+    char * pathname = NULL;
+
+    char hookpath[256];
+    memset(hookpath, 0, 256);
+
+    if (strendswith(path, "wine-preloader"))
+    {
+        strcat(hookpath, path);
+        strcat(hookpath, "_hook");
+        
+        wineloader = hookpath;
+    }
+
     new_argv[0] = wineloader;
-
     int res = old_execv(wineloader, new_argv);
-
     free( new_argv );
 
     return res;
