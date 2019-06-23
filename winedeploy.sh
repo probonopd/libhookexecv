2ce5e92f370ada279fe436418be1507f416e1f86
probonopd <probonopd@users.noreply.github.com>
2019-06-23 08:10:03 +0000

    Update winedeploy.sh

--- winedeploy.sh
+++ winedeploy.sh
@@ -65,7 +65,7 @@ wget -c "https://github.com/$DLD"
 tar xf dxvk-*.tar.gz
 cp ./setup_dxvk.sh ./dxvk-*/
 
-sed -i -e 's|^Name=.*|Name=Proton with DXVK|g' ./Wine.AppDir/*.desktop
+sed -i -e 's|^Name=.*|Name=Proton with DXVK|g' ./Wine.AppDir/usr/share/applications/wine.desktop
 
 ##################################################################
 
